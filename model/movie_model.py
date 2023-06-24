d4c32a8e68e94ec88ef0c0fcda233d0dddc348b8
Chris Bidlake <cbidlake@live.com>
2023-06-23 23:12:33 -0400

    Update model pkl imports

--- model/movie_model.py
+++ model/movie_model.py
@@ -19,8 +19,8 @@ class RatingModel():
     """Class to evaluate reviews using the movie model"""
     # def __init__(self):
         # Load model and vectorizer
-    nb_model = pickle.load(open(r'model\nb_model.pkl','rb'))
-    nb_vectorizer = pickle.load(open(r'model\nb_vectorizer.pkl','rb'))
+    nb_model = pickle.load(open(r'\model\nb_model.pkl','rb'))
+    nb_vectorizer = pickle.load(open(r'\model\nb_vectorizer.pkl','rb'))
 
     def eval(self, user_input):
         """Default query to evaluate a submitted review"""
